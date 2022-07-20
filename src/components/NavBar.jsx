0bde6ba868cb6db75c90b8539e5c35b857aa0bb7
Cesar Aular <cesar.aularb@gmail.com>
2022-07-19 22:56:57 -0300

    navstyles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <div>
         <nav className="nav">
-         <a href="#" className="brand">aular ecommerce</a>
+         <a href="#" className="nav__brand">Ecommerce</a>
          <ul className="nav__menu">
             <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
             <li className="nav__item"><a href="#" className="nav__link">About us</a></li>
