4a1853177f63a50f86b4b5c99e8f14a01b7230f2
Cesar Aular <cesar.aularb@gmail.com>
2022-07-21 22:12:49 -0300

    cambios

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ItemListContainer = () => {
+  return (
+    <>
+    <div className='products-heading'> 
+    Seller Products 
+    </div>
+    <p className='text'>Everything all you need </p>
+
+    <div>Product one</div>
+    <div>Product two</div>
+    <div>Product three</div>
+    </>
+  )
+}
+
+export default ItemListContainer
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,15 +4,21 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div>
-        <nav className="nav">
-         <a href="#" className="nav__brand">Ecommerce</a>
-         <ul className="nav__menu">
-            <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
-            <li className="nav__item"><a href="#" className="nav__link">About us</a></li>
-            <li className="nav__item"><a href="#" className="nav__link">Shop</a></li>
-            <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
-         </ul>
-        </nav>
+      <nav className="nav">
+        <a href="#" className="nav__brand">Ecommerce</a>
+        <ul className="nav__menu">
+          <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
+          <li className="nav__item"><a href="#" className="nav__link">About us</a></li>
+          <li className="nav__item"><a href="#" className="nav__link">Shop</a></li>
+          <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
+        </ul>
+        <div className='nav__toggler'>
+           <div className="line1"></div>
+           <div className="line2"></div>
+           <div className="line3"></div>
+        </div>
+      </nav>
+    
     </div>
   )
 }
